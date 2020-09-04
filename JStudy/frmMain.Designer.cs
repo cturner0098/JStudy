5f3df3a1f1a59c02cf384159cf1a6afe2c541144
turnerc <turnerc@LEARN21-77656>
2020-09-04 12:22:34 -0400

    Add project files.

---
+++ JStudy/frmMain.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace JStudy
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(12, 12);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(662, 307);
+            this.rtbOutput.TabIndex = 0;
+            this.rtbOutput.Text = "";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbOutput);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbOutput;
+    }
+}
+
---
+++ JStudy/frmMain.cs
@@ -0,0 +1,55 @@
+﻿//https://docs.microsoft.com/en-us/dotnet/api/system.net.httpwebrequest.headers?view=netcore-3.1
+//https://stackoverflow.com/questions/40402782/how-to-pass-api-key-through-httpwebrequest/40402986
+//https://docs.api.wanikani.com/20170710/?javascript#get-all-reviews
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JStudy
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            string wanikaniRevision = "20170710";
+            string apiKey = "500ce2a7-c0b6-4361-b4fa-c74081b123ca";
+            string authHeader = "Bearer " + apiKey;
+            string apiEndpoint = "reviews";
+            HttpWebRequest client = (HttpWebRequest)WebRequest.Create("https://api.wanikani.com/v2/" + apiEndpoint);
+            client.Headers["Authorization"] = authHeader;
+            client.Headers["Wanikani-Revision"] = wanikaniRevision;
+
+            HttpWebResponse clientResposne = (HttpWebResponse)client.GetResponse();
+
+            Stream streamResposne = clientResposne.GetResponseStream();
+            StreamReader streamRead = new StreamReader(streamResposne);
+            Char[] readBuff = new char[256];
+            int count = streamRead.Read(readBuff, 0, 256);
+            while(count > 0)
+            {
+                String outputData = new string(readBuff, 0, count);
+                rtbOutput.AppendText("Line " + count + " " + outputData);
+                count = streamRead.Read(readBuff, 0, 256);
+            }
+
+            streamResposne.Close();
+            streamRead.Close();
+            // Release the HttpWebResponse Resource.
+            clientResposne.Close();
+        }
+    }
+}
